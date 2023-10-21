d662c1a854b86c33d0ecbef261495fe319ba0d96
omer can boran <omerboran22@gmail.com>
2023-10-21 17:22:45 +0300

    commint

---
+++ eskiser.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/uploadsFileList', (req, res) => {
+  fs.readdir(path.join(__dirname, './uploads'), (err, dosyalar) => {
+    if (err) {
+      res.status(500).send('Klasör okunurken hata oluştu');
+      return;
+    }
+    var uploadFiles = [];
+    dosyalar.map((dosya) => {
+      uploadFiles.push({ dosya });
+    });
+
+    res.json(dosyalar);
+  });
+});
+
+const hedefDizin = path.join(__dirname, './uploads');
+
+// Multer ayarları
+const yukleme = multer({ dest: hedefDizin });
+
+app.post('/dosya-yukle', yukleme.single('dosya'), (req, res) => {
+  const yuklenenDosya = req.file;
+
+  if (!yuklenenDosya) {
+    return res.status(400).send('Dosya yüklenemedi.');
+  }
+
+  // Orijinal dosya adını al
+  const orijinalDosyaAdi = req.file.originalname;
+
+  // Yeni dosya yolu ve adını oluştur
+  const hedefDizin = 'uploads/';
+  const yeniDosyaYolu = path.join(__dirname, hedefDizin, orijinalDosyaAdi);
+
+  fs.rename(yuklenenDosya.path, yeniDosyaYolu, (err) => {
+    if (err) {
+      console.error('Dosya kaydedilirken hata oluştu:', err);
+      return res.status(500).send('Dosya kaydedilemedi.');
+    }
+    res.send(`Dosya başarıyla kaydedildi: ${yeniDosyaYolu}`);
+  });
+});
+
+app.get('/listele:dosyaYolu(*)', (req, res) => {
+  const dosyaYolu = path.join('li/', req.params.dosyaYolu);
+  const dosyaIcerigi = listeleKlasorIcerigi(dosyaYolu);
+
+  let icerik = '';
+
+  dosyaIcerigi.forEach((dosya) => {
+    if (dosya.tip === 'klasor') {
+      icerik += `<a href="/${dosya.url}"> ${dosya.isim}</a>`;
+    } else if (dosya.tip === 'dosya') {
+      icerik += `<a href="/${dosya.url}">${dosya.isim}</a>`;
+    }
+  });
+
+  res.send(icerik);
+});
+
+function listeleKlasorIcerigi(dizinYolu) {
+  console.log(dizinYolu);
+  const dosyalar = fs.readdirSync(dizinYolu);
+
+  return dosyalar
+    .map((dosya) => {
+      const dosyaYolu = path.join(dizinYolu, dosya);
+      const istatistik = fs.statSync(dosyaYolu);
+
+      if (istatistik.isDirectory()) {
+        return {
+          isim: dosya,
+          tip: 'klasor',
+          url: dosyaYolu,
+          altDosyalar: listeleKlasorIcerigi(dosyaYolu),
+        };
+      } else if (istatistik.isFile() && dosyaYolu.endsWith('.html')) {
+        return {
+          isim: dosya,
+          tip: 'dosya',
+          url: dosyaYolu, // HTML dosyalarını tarayıcıda açmak için yolu ekliyoruz
+        };
+      }
+
+      return null;
+    })
+    .filter(Boolean);
+}
+
+app.get('app/:klasorAdi/:dosyaAdi', (req, res) => {
+  const { klasorAdi, dosyaAdi } = req.params;
+  const dosyaYolu = path.join(klasorAdi, 'app/' + dosyaAdi);
+
+  console.log(dosyaYolu);
+  res.sendFile(dosyaYolu, { root: __dirname }, (err) => {
+    if (err) {
+      res.status(500).send('Dosya gönderilirken hata oluştu');
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor`);
+});
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const app = express();
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/uploadsFileList', (req, res) => {
+  fs.readdir(path.join(__dirname, './uploads'), (err, dosyalar) => {
+    if (err) {
+      res.status(500).send('Klasör okunurken hata oluştu');
+      return;
+    }
+    var uploadFiles = [];
+    dosyalar.map((dosya) => {
+      uploadFiles.push({ dosya });
+    });
+
+    res.json(dosyalar);
+  });
+});
+
+const hedefDizin = path.join(__dirname, './uploads');
+
+// Multer ayarları
+const yukleme = multer({ dest: hedefDizin });
+
+app.post('/dosya-yukle', yukleme.single('dosya'), (req, res) => {
+  const yuklenenDosya = req.file;
+
+  if (!yuklenenDosya) {
+    return res.status(400).send('Dosya yüklenemedi.');
+  }
+
+  // Orijinal dosya adını al
+  const orijinalDosyaAdi = req.file.originalname;
+
+  // Yeni dosya yolu ve adını oluştur
+  const hedefDizin = 'uploads/';
+  const yeniDosyaYolu = path.join(__dirname, hedefDizin, orijinalDosyaAdi);
+
+  fs.rename(yuklenenDosya.path, yeniDosyaYolu, (err) => {
+    if (err) {
+      console.error('Dosya kaydedilirken hata oluştu:', err);
+      return res.status(500).send('Dosya kaydedilemedi.');
+    }
+    res.send(`Dosya başarıyla kaydedildi: ${yeniDosyaYolu}`);
+  });
+});
+
+// Statik dosyaları sunmak için express.static kullanıyoruz
+app.use('/app', express.static(path.join(__dirname, 'app')));
+
+// Klasör içeriğini listeleme ve dosyaları açma endpoint'i
+
+app.get('/listele-ve-ac/:klasorYolu(*)', (req, res) => {
+  const klasorYolu = path.join(__dirname, 'app', req.params.klasorYolu);
+
+  fs.readdir(klasorYolu, (err, dosyalar) => {
+    if (err) {
+      return res.status(500).send('Klasör içeriği listelenirken hata oluştu.');
+    }
+
+    let icerik = '';
+
+    dosyalar.forEach((dosya) => {
+      const dosyaYolu = path.join(klasorYolu, dosya);
+      const istatistik = fs.statSync(dosyaYolu);
+
+      if (istatistik.isDirectory()) {
+        icerik += `<p><a href="/listele-ve-ac/${req.params.klasorYolu}/${dosya}"><strong>Klasör: ${dosya}</strong></a></p>`;
+      } else if (istatistik.isFile() && dosyaYolu.endsWith('.html')) {
+        icerik += `<p><a href="/app/${req.params.klasorYolu}/${dosya}">${dosya}</a></p>`;
+      }
+    });
+
+    res.send(icerik);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor`);
+});
